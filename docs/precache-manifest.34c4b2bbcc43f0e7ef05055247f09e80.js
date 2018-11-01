34a9b91c4348d968a709c9f2691ebd507bed66ab
Nezirić Aldin <neziric.aldin@nsoft.com>
2018-11-02 00:13:27 +0100

    Replace build output folder to match github pages

---
+++ docs/precache-manifest.34c4b2bbcc43f0e7ef05055247f09e80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "356f73f0bb8b8d3d6099",
+    "url": "/static/js/main.356f73f0.chunk.js"
+  },
+  {
+    "revision": "d5e90d50b378394d9d5b",
+    "url": "/static/js/1.d5e90d50.chunk.js"
+  },
+  {
+    "revision": "356f73f0bb8b8d3d6099",
+    "url": "/static/css/main.44fc32f0.chunk.css"
+  },
+  {
+    "revision": "12b8726686573640db9d99e54740a213",
+    "url": "/index.html"
+  }
+];
